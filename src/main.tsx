e0a3402bc87f59aa698f744bd63ee028ce930615
Mohammed Khallaf 🏠 <mohammedkhallaf.dev@gmail.com>
2026-03-24 11:30:20 +0200

    feat: sync stored language to i18next on boot

--- src/main.tsx
+++ src/main.tsx
@@ -4,25 +4,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { useStore } from './store/store'
+import i18n from './i18n'
+
+// Sync stored language to i18next on boot
+const { language, notificationTime, notificationPermission } = useStore.getState()
+if (language !== 'ar') {
+  i18n.changeLanguage(language)
+  document.documentElement.lang = language
+}
 
 // Schedule daily notification if permission granted and time set
 function scheduleNotification() {
-  const { notificationTime, notificationPermission, language } = useStore.getState()
   if (!notificationTime || notificationPermission !== 'granted') return
   const [hh, mm] = notificationTime.split(':').map(Number)
   const now = new Date()
   const target = new Date()
   target.setHours(hh, mm, 0, 0)
   if (target <= now) target.setDate(target.getDate() + 1)
-  const ms = target.getTime() - now.getTime()
   setTimeout(() => {
     new Notification('قضاء', {
       body: language === 'ar' ? 'لا تنسَ قضاءك اليوم 🌙' : "Don't forget your Qadaa today 🌙",
       icon: '/icons/icon-192.png',
     })
-  }, ms)
+  }, target.getTime() - now.getTime())
 }
-
 scheduleNotification()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
